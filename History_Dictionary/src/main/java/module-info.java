47d36aeff66f9dd49497db3bd05349884494e377
WahajT <wahajtahir01@gamil.com>
2021-12-30 00:55:52 +0500

    Done

--- History_Dictionary/src/main/java/module-info.java
+++
@@ -1,6 +0,0 @@
-module SoftwareConstruction.History_Dictionary {
-    requires javafx.controls;
-	requires javafx.graphics;
-	requires java.sql;
-    exports SoftwareConstruction.History_Dictionary;
-}
